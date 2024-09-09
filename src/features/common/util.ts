83fc0a827ee53e2c1c075f59bddd2624dd5f5a51
Mathiasfx <mathias_fx@yahoo.com.ar>
2024-09-09 10:02:53 -0300

    cambio el numero del alfabeto

--- src/features/common/util.ts
+++ src/features/common/util.ts
@@ -3,6 +3,6 @@ import { customAlphabet } from "nanoid";
 export const uniqueId = () => {
   const alphabet =
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  const nanoid = customAlphabet(alphabet, 37);
+  const nanoid = customAlphabet(alphabet, 36);
   return nanoid();
 };
